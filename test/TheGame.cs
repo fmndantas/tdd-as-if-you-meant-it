782a01e17b5df281c3534659118e02c4db702f1a
Fernando <fernandodantas1996@gmail.com>
2023-06-13 15:00:08 -0300

    failing
    missing alternations between `CirclesPlays` and `CrossPlays`

--- test/TheGame.cs
+++ test/TheGame.cs
@@ -7,7 +7,17 @@ public class TheGame
     {
         Assert.That(State(new int[] { }), Is.EqualTo(GameStatus.CrossPlays));
     }
-    
+
+    [Test]
+    public void AlternatesBetweenStatusCrossPlaysAndCirclesPlaysAfterEachMoveUntilGameIsFinished()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(State(new[] { 0 }), Is.EqualTo(GameStatus.CirclesPlays));
+            Assert.That(State(new[] { 0, 1 }), Is.EqualTo(GameStatus.CrossPlays));
+        });
+    }
+
     private GameStatus State(int[] moves)
     {
         return GameStatus.CrossPlays;
