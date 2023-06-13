34a5468b01d9e1814adf4aaf359e25dbadbedf61
Fernando <fernandodantas1996@gmail.com>
2023-06-13 15:04:24 -0300

    passing
    changes `State` to consider alternating between `CrossPlays` and `CirclePlays`

--- test/TheGame.cs
+++ test/TheGame.cs
@@ -9,22 +9,23 @@ public class TheGame
     }
 
     [Test]
-    public void AlternatesBetweenStatusCrossPlaysAndCirclesPlaysAfterEachMoveUntilGameIsFinished()
+    public void AlternatesBetweenStatusCrossPlaysAndCirclePlaysAfterEachMoveUntilGameIsFinished()
     {
         Assert.Multiple(() =>
         {
-            Assert.That(State(new[] { 0 }), Is.EqualTo(GameStatus.CirclesPlays));
+            Assert.That(State(new[] { 0 }), Is.EqualTo(GameStatus.CirclePlays));
             Assert.That(State(new[] { 0, 1 }), Is.EqualTo(GameStatus.CrossPlays));
         });
     }
 
     private GameStatus State(int[] moves)
     {
-        return GameStatus.CrossPlays;
+        return moves.Length % 2 == 0 ? GameStatus.CrossPlays : GameStatus.CirclePlays;
     }
 }
 
 enum GameStatus
 {
-    CrossPlays
+    CrossPlays,
+    CirclePlays
 }
