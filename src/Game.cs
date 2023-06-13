324974222237046782be1359f244309fca21fbf5
Fernando <fernandodantas1996@gmail.com>
2023-06-13 19:18:53 -0300

    passing
    game can identify this column filled

--- src/Game.cs
+++ src/Game.cs
@@ -9,7 +9,7 @@ public class Game
         options = new List<HashSet<int>>
         {
             new() { 0, 1, 2 }, new() { 3, 4, 5 }, new() { 6, 7, 8 },
-            new() { 0, 3, 6 }, new() { 1, 4, 7 }
+            new() { 0, 3, 6 }, new() { 1, 4, 7 }, new() { 2, 5, 8 }
         };
     }
 
