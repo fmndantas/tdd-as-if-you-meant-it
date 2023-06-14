f5f06174d7f6df9e07da2f233f88cb8a482fd197
Fernando <fernandodantas1996@gmail.com>
2023-06-14 08:55:49 -0300

    failing
    introduces square grid size

--- test/TheGame.cs
+++ test/TheGame.cs
@@ -7,14 +7,14 @@ public class TheGame
     [Test]
     public void AssumesStatusCrossPlaysWhenNoMovesArePerformed()
     {
-        var game = new Game();
+        var game = new Game(3);
         Assert.That(game.State(new int[] { }), Is.EqualTo(GameState.CrossPlays));
     }
 
     [Test]
     public void AlternatesBetweenStatusCrossPlaysAndCirclePlaysAfterEachMoveUntilGameIsFinished()
     {
-        var game = new Game();
+        var game = new Game(3);
         Assert.Multiple(() =>
         {
             Assert.That(game.State(new[] { 0 }), Is.EqualTo(GameState.CirclePlays));
@@ -28,7 +28,7 @@ public class TheGame
     [TestCase(new[] { 0, 8, 1, 6, 3, 7 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsAnEntireRow(int[] moves)
     {
-        return new Game().State(moves);
+        return new Game(3).State(moves);
     }
 
     [TestCase(new[] { 0, 1, 3, 2, 6 }, ExpectedResult = GameState.CrossWon)]
@@ -37,7 +37,7 @@ public class TheGame
     [TestCase(new[] { 1, 0, 2, 3, 8, 6 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsAnEntireColumn(int[] moves)
     {
-        return new Game().State(moves);
+        return new Game(3).State(moves);
     }
 
     [TestCase(new[] { 0, 6, 8, 7, 4 }, ExpectedResult = GameState.CrossWon)]
@@ -45,20 +45,20 @@ public class TheGame
     [TestCase(new[] { 1, 2, 7, 4, 0, 6 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsOneOfTwoDiagonals(int[] moves)
     {
-        return new Game().State(moves);
+        return new Game(3).State(moves);
     }
 
     [TestCase(new[] { 0, 1, 2, 4, 3, 5, 8, 6, 7 }, ExpectedResult = GameState.Tied)]
     [TestCase(new[] { 0, 2, 1, 3, 4, 7, 5, 8, 6 }, ExpectedResult = GameState.Tied)]
     public GameState InformsThatGameIsTiedWhenAllSpotsAreFilledAndNoPlayerHasWon(int[] moves)
     {
-        return new Game().State(moves);
+        return new Game(3).State(moves);
     }
 
     [TestCase(new[] { 0, 1, 3, 4, 6, 7 })]
     [TestCase(new[] { 0, 3, 1, 4, 2, 5 })]
     public void ThrowErrorWhenMoreThanOneWayToWinAreSpotted(int[] moves)
     {
-        Assert.Throws<GameInInvalidState>(() => { new Game().State(moves); });
+        Assert.Throws<GameInInvalidState>(() => { new Game(3).State(moves); });
     }
 }
