0b6ec2e3bdabfd2a600a79026b6ac90cf4daa01c
Fernando <fernandodantas1996@gmail.com>
2023-06-14 08:43:09 -0300

    passing
    game can identify second diagonal filled

--- test/TheGame.cs
+++ test/TheGame.cs
@@ -42,6 +42,7 @@ public class TheGame
 
     [TestCase(new[] { 0, 6, 8, 7, 4 }, ExpectedResult = GameState.CrossWon)]
     [TestCase(new[] { 4, 5, 2, 8, 6 }, ExpectedResult = GameState.CrossWon)]
+    [TestCase(new[] { 1, 2, 7, 4, 0, 6 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsOneOfTwoDiagonals(int[] moves)
     {
         return new Game().State(moves);
