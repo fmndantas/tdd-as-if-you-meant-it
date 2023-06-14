7dcb7b4ab13a3a85ecfc02fa97db3314fcde171a
Fernando <fernandodantas1996@gmail.com>
2023-06-14 09:25:31 -0300

    failing
    delegates options to generator. Tests are falling because not all patterns are being generated

--- src/Game.cs
+++ src/Game.cs
@@ -6,12 +6,7 @@ public class Game
 
     public Game(int n, WinnerPatternsGenerator generator)
     {
-        _options = new List<HashSet<int>>
-        {
-            new() { 0, 1, 2 }, new() { 3, 4, 5 }, new() { 6, 7, 8 },
-            new() { 0, 3, 6 }, new() { 1, 4, 7 }, new() { 2, 5, 8 },
-            new() { 0, 4, 8 }, new() { 6, 4, 2 }
-        };
+        _options = generator.Generate(n);
     }
 
     public GameState State(int[] moves)
--- test/TheGame.cs
+++ test/TheGame.cs
@@ -7,14 +7,14 @@ public class TheGame
     [Test]
     public void AssumesStatusCrossPlaysWhenNoMovesArePerformed()
     {
-        var game = new Game(3);
+        var game = new Game(3, new WinnerPatternsGenerator());
         Assert.That(game.State(new int[] { }), Is.EqualTo(GameState.CrossPlays));
     }
 
     [Test]
     public void AlternatesBetweenStatusCrossPlaysAndCirclePlaysAfterEachMoveUntilGameIsFinished()
     {
-        var game = new Game(3);
+        var game = new Game(3, new WinnerPatternsGenerator());
         Assert.Multiple(() =>
         {
             Assert.That(game.State(new[] { 0 }), Is.EqualTo(GameState.CirclePlays));
@@ -26,7 +26,7 @@ public class TheGame
     [TestCase(new[] { 0, 3, 2, 4, 1 }, 3, ExpectedResult = GameState.CrossWon)]
     [TestCase(new[] { 0, 4, 2, 3, 8, 5 }, 3, ExpectedResult = GameState.CircleWon)]
     [TestCase(new[] { 0, 8, 1, 6, 3, 7 }, 3, ExpectedResult = GameState.CircleWon)]
-    [TestCase(new[] { 0, 12, 1, 13, 2, 14, 3 }, 4, ExpectedResult = GameState.CrossWon)]
+    [TestCase(new[] { 0, 12, 2, 13, 3, 14, 4, 15 }, 4, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsAnEntireRow(int[] moves, int n)
     {
         return new Game(n, new WinnerPatternsGenerator()).State(moves);
@@ -38,7 +38,7 @@ public class TheGame
     [TestCase(new[] { 1, 0, 2, 3, 8, 6 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsAnEntireColumn(int[] moves)
     {
-        return new Game(3).State(moves);
+        return new Game(3, new WinnerPatternsGenerator()).State(moves);
     }
 
     [TestCase(new[] { 0, 6, 8, 7, 4 }, ExpectedResult = GameState.CrossWon)]
@@ -46,20 +46,20 @@ public class TheGame
     [TestCase(new[] { 1, 2, 7, 4, 0, 6 }, ExpectedResult = GameState.CircleWon)]
     public GameState InformsThatCurrentPlayerWinsIfThisPlayerFillsOneOfTwoDiagonals(int[] moves)
     {
-        return new Game(3).State(moves);
+        return new Game(3, new WinnerPatternsGenerator()).State(moves);
     }
 
     [TestCase(new[] { 0, 1, 2, 4, 3, 5, 8, 6, 7 }, ExpectedResult = GameState.Tied)]
     [TestCase(new[] { 0, 2, 1, 3, 4, 7, 5, 8, 6 }, ExpectedResult = GameState.Tied)]
     public GameState InformsThatGameIsTiedWhenAllSpotsAreFilledAndNoPlayerHasWon(int[] moves)
     {
-        return new Game(3).State(moves);
+        return new Game(3, new WinnerPatternsGenerator()).State(moves);
     }
 
     [TestCase(new[] { 0, 1, 3, 4, 6, 7 })]
     [TestCase(new[] { 0, 3, 1, 4, 2, 5 })]
     public void ThrowErrorWhenMoreThanOneWayToWinAreSpotted(int[] moves)
     {
-        Assert.Throws<GameInInvalidState>(() => { new Game(3).State(moves); });
+        Assert.Throws<GameInInvalidState>(() => { new Game(3, new WinnerPatternsGenerator()).State(moves); });
     }
 }
