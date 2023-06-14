e65fcef704315e51a415c66ab7914263af504a27
Fernando <fernandodantas1996@gmail.com>
2023-06-14 10:10:51 -0300

    passing

--- src/WinnerPatternsGenerator.cs
+++ src/WinnerPatternsGenerator.cs
@@ -4,7 +4,14 @@ public class WinnerPatternsGenerator
 {
     public List<HashSet<int>> Generate(int n)
     {
-        return GenerateRowOptions(n);
+        var rows = GenerateRowOptions(n);
+        var columns = GenerateColumnsOptions(n);
+        var diagonals = GenerateDiagonalOptions(n);
+        var result = new List<HashSet<int>>();
+        result.AddRange(rows);
+        result.AddRange(columns);
+        result.AddRange(diagonals);
+        return result;
     }
 
     private List<HashSet<int>> GenerateRowOptions(int n)
@@ -15,12 +22,46 @@ public class WinnerPatternsGenerator
             var option = new HashSet<int>();
             for (var col = 0; col < n; col++)
             {
-                option.Add(col * (row + 1));
+                option.Add(row * n + col);
             }
 
             options.Add(option);
         }
 
         return options;
+    }
+
+    private List<HashSet<int>> GenerateColumnsOptions(int n)
+    {
+        var options = new List<HashSet<int>>();
+        for (var col = 0; col < n; col++)
+        {
+            var option = new HashSet<int>();
+            for (var row = 0; row < n; row++)
+            {
+                option.Add(col + n * row);
+            }
+
+            options.Add(option);
+        }
+
+        return options;
+    }
+
+    private List<HashSet<int>> GenerateDiagonalOptions(int n)
+    {
+        var firstDiagonal = new HashSet<int>();
+        for (var i = 0; i < n; i++)
+        {
+            firstDiagonal.Add(n * i + i);
+        }
+
+        var secondDiagonal = new HashSet<int>();
+        for (int row = n - 1, col = 0; row >= 0; row--, col++)
+        {
+            secondDiagonal.Add(row * n + col);
+        }
+
+        return new List<HashSet<int>> { firstDiagonal, secondDiagonal };
     }
 }
--- test/TheWinnerPatternsGenerator.cs
+++ test/TheWinnerPatternsGenerator.cs
@@ -12,6 +12,34 @@ public class TheWinnerPatternsGenerator
         Assert.Multiple(() =>
         {
             Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 1, 2 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 3, 4, 5 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 6, 7, 8 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 3, 6 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 1, 4, 7 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 2, 5, 8 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 4, 8 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 6, 4, 2 }));
+        });
+    }
+
+    [Test]
+    public void GeneratesRowPatternsForGridWithSize4()
+    {
+        var generator = new WinnerPatternsGenerator();
+        var patterns = generator.Generate(4);
+        Assert.Multiple(() =>
+        {
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 1, 2, 3 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 4, 5, 6, 7 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 8, 9, 10, 11 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 12, 13, 14, 15 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 4, 8, 12 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 1, 5, 9, 13 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 2, 6, 10, 14 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 3, 7, 11, 15 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 3, 7, 11, 15 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 0, 5, 10, 15 }));
+            Assert.That(patterns, Contains.Item(new HashSet<int> { 12, 9, 6, 3 }));
         });
     }
 }
